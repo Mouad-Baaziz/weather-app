a24c2f79a1fd952ccb6101a66f67b3287e3a2200
mouad <mouad.04ba@gmail.com>
2025-11-23 12:50:25 +0100

    Fix Procfile with correct Django project name

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -137,3 +137,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-production-58c4.up.railway.app",
+]
